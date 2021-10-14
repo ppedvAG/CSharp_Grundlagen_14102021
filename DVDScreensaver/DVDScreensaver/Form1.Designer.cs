8a87886e20d5ae9cfdd72ef680cedfead89584f8
André <ruland@outlook.de>
2021-10-14 16:19:03 +0200

    Dvd Screensaver ist toll

---
+++ DVDScreensaver/DVDScreensaver/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace DVDScreensaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Image = global::DVDScreensaver.Properties.Resources.cat;
+            this.button1.Location = new System.Drawing.Point(147, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 117);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(924, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DVDScreensaver/DVDScreensaver/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVDScreensaver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int speedX = 3;
+        int speedY = 3;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.Left += speedX;
+            button1.Top += speedY;
+
+            if (button1.Left + button1.Width >= ClientRectangle.Width ||
+                button1.Left <= 0)
+            {
+                speedX *= -1;
+                MachWas();
+            }
+
+            if (button1.Top + button1.Height >= ClientRectangle.Height ||
+                button1.Top <= 0)
+            {
+                speedY *= -1;
+                MachWas();
+            }
+        }
+
+        void MachWas()
+        {
+            Console.Beep(400, 600);
+
+        }
+
+    }
+}
