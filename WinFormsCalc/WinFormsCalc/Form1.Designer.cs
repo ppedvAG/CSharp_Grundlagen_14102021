0d0051080938c98208530439557078926288cd24
André <ruland@outlook.de>
2021-10-14 15:28:16 +0200

    Calc mit Winforms

---
+++ WinFormsCalc/WinFormsCalc/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace WinFormsCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.boolAndLabel = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.boolOrLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.inkLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 39);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 39);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Berechnen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ergebnis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "+";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(344, 35);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(170, 39);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(344, 146);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(170, 39);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Berechnen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ergebnis";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(56, 38);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(159, 36);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(56, 90);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(159, 36);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 46);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "AND";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // boolAndLabel
+            // 
+            this.boolAndLabel.AutoSize = true;
+            this.boolAndLabel.Location = new System.Drawing.Point(8, 216);
+            this.boolAndLabel.Name = "boolAndLabel";
+            this.boolAndLabel.Size = new System.Drawing.Size(161, 32);
+            this.boolAndLabel.TabIndex = 10;
+            this.boolAndLabel.Text = "boolAndLabel";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(211, 167);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 46);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "OR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // boolOrLabel
+            // 
+            this.boolOrLabel.AutoSize = true;
+            this.boolOrLabel.Location = new System.Drawing.Point(199, 216);
+            this.boolOrLabel.Name = "boolOrLabel";
+            this.boolOrLabel.Size = new System.Drawing.Size(144, 32);
+            this.boolOrLabel.TabIndex = 10;
+            this.boolOrLabel.Text = "boolOrLabel";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.boolOrLabel);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.boolAndLabel);
+            this.groupBox1.Location = new System.Drawing.Point(610, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 277);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Spass mit Bools";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.inkLabel);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(1047, 24);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(384, 238);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Inkrementieren";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(20, 55);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 46);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // inkLabel
+            // 
+            this.inkLabel.AutoSize = true;
+            this.inkLabel.Location = new System.Drawing.Point(118, 62);
+            this.inkLabel.Name = "inkLabel";
+            this.inkLabel.Size = new System.Drawing.Size(102, 32);
+            this.inkLabel.TabIndex = 1;
+            this.inkLabel.Text = "inkLabel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1598, 328);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label boolAndLabel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label boolOrLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label inkLabel;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WinFormsCalc/WinFormsCalc/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsCalc
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textDerTextBox1 = textBox1.Text;
+            string textDerTextBox2 = textBox2.Text;
+
+            if (!int.TryParse(textDerTextBox1, out int zahl1))
+            {
+                MessageBox.Show("Zahl 1 konnte nicht eingelesen werden");
+                return;
+            }
+
+            int zahl2;
+            if (!int.TryParse(textDerTextBox2, out zahl2))
+            {
+                MessageBox.Show("Zahl 2 konnte nicht eingelesen werden");
+                return;
+            }
+
+            int ergebnis = zahl1 + zahl2;
+
+            label1.Text = ergebnis.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decimal result = numericUpDown1.Value + numericUpDown2.Value;
+            label3.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool statusVonCheckBox1 = checkBox1.Checked;
+            bool statusVonCheckBox2 = checkBox2.Checked;
+
+            bool result = statusVonCheckBox1 && statusVonCheckBox2;
+
+            boolAndLabel.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool result = checkBox1.Checked || checkBox2.Checked;
+
+            boolOrLabel.Text = result.ToString();
+
+        }
+
+        int zähler = 0;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //zähler = zähler + 1;
+            zähler += 2;
+            //zähler++;
+
+            inkLabel.Text = zähler.ToString();
+        }
+    }
+}
