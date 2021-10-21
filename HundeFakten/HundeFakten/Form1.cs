e5165fd6aa44c9cdb19f30ac6502262da6a32852
André <ruland@outlook.de>
2021-10-21 12:51:51 +0200

    Hundefakten mit und ohne Proxy

---
+++ HundeFakten/HundeFakten/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace HundeFakten
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+            string url = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1&index=1";
+
+            //mit Proxy
+            //var handler = new HttpClientHandler();
+            //handler.DefaultProxyCredentials = CredentialCache.DefaultCredentials;
+            //HttpClient.DefaultProxy = new WebProxy("http://myFFFProxy:80", true);
+            //HttpClient http = new HttpClient(handler);
+
+            //ohne Proxy
+            HttpClient http = new HttpClient();
+
+            http.Timeout = TimeSpan.FromSeconds(5); //damit der fehler schneller kommt 
+
+            string json = await http.GetStringAsync(url);
+
+            listBox1.Items.Insert(0, json);
+
+            button1.Enabled = true;
+        }
+    }
+}
