8664198fc1d1de2aa3c12c443bdac5d5077a4f2c
André <ruland@outlook.de>
2021-10-22 12:01:57 +0200

    Interfaces sind auch gant Nett

--- HalloPoly/HalloPoly/Lebewesen.cs
+++ HalloPoly/HalloPoly/Lebewesen.cs
@@ -1,15 +1,61 @@
 ﻿namespace HalloPoly
 {
-    class Lebewesen
+    abstract class Lebewesen
     {
+        public abstract string SagMitDeinenName();
+
         public virtual string WasBinIch()
         {
             return "Ich bin ein Lebewesen";
         }
     }
 
-    class Mensch : Lebewesen
+    interface ICanSpeak
     {
+        string Sprache { get; }
+        string Sprechen(string text);
+    }
+
+    class Vogel : Lebewesen, ICanSpeak
+    {
+        public string Sprache { get { return "Piepisch"; } }
+
+        public override string SagMitDeinenName()
+        {
+            return "Rudi";
+        }
+
+        public string Sprechen(string text)
+        {
+            return $"BRAAAAA {text}";
+        }
+    }
+
+    class Baum : Lebewesen
+    {
+        public override string SagMitDeinenName()
+        {
+            return "Walter";
+        }
+    }
+
+    class Mensch : Lebewesen, ICanSpeak
+    {
+        public string Sprache
+        {
+            get { return "DE"; }
+        }
+
+        public override string SagMitDeinenName()
+        {
+            return "Fred";
+        }
+
+        public string Sprechen(string text)
+        {
+            return $"BLA BLA BLA {text}";
+        }
+
         public override string WasBinIch()
         {
             return "Ich bin ein Mensch";
--- HalloPoly/HalloPoly/Program.cs
+++ HalloPoly/HalloPoly/Program.cs
@@ -8,19 +8,37 @@ namespace HalloPoly
         {
             Console.WriteLine("Hello World!");
 
-            Lebewesen lw = new Lebewesen();
-            ZeigeWasBinIch(lw);
+            //geht nicht mehr weil die klasse nun abstract ist
+            //Lebewesen lw = new Lebewesen();
+            //ZeigeWasBinIch(lw);
 
             Mensch m = new Mensch();
             ZeigeWasBinIch(m);
 
-                
+            Baum b = new Baum();
+            ZeigeWasBinIch(b);
+
+            Vogel v = new Vogel();
+            ZeigeWasBinIch(v);
+
+            Sprich(v);
+            Sprich(m);
+            //Sprich(b);
+
+
             Console.WriteLine("Ende");
             Console.ReadLine();
         }
+
+        static void Sprich(ICanSpeak speaker)
+        {
+            Console.WriteLine($"{speaker.Sprache }: {speaker.Sprechen("Hallo")}");
+        }
+
         static void ZeigeWasBinIch(Lebewesen lw)
         {
-            Console.WriteLine(lw.WasBinIch());
+            Console.WriteLine($"{lw.WasBinIch()} " +
+                $"mit dem Namen {lw.SagMitDeinenName()}");
         }
     }
 }
